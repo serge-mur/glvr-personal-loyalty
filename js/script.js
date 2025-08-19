295773c902320b085ba183ec401e93e26c6cf1e3
Serge <serge-mur@yandex.ru>
2025-08-19 16:05:41 +0300

    card

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const questions = document.querySelectorAll('.faq__question');
+  
+    questions.forEach(question => {
+        question.addEventListener('click', function() {
+            this.classList.toggle('active');
+            this.nextElementSibling.classList.toggle('show');
+        });
+    });
+
 });
